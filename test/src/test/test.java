3f4321f9c287650e82bf55d43b821207b10be048
bogiy <bogiy@DESKTOP-FAHJV5N>
2018-10-29 15:37:09 +0900

    pushhhhhh

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,7 +5,6 @@ public class test {
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World");
-		System.out.println("Hello World");
-			
+		System.out.println("Hello World");		
 	}
 }
