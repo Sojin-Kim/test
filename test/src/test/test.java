ba54d7399aa705f8862eda04ae2d49bf1c823009
bogiy <bogiy@DESKTOP-FAHJV5N>
2018-10-29 14:51:16 +0900

    hello world 여러줄 추가

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -3,5 +3,9 @@ package test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+			
 	}
 }
